82e5ee9c1ef3b492f7b28e2b484eca05506d4750
R Santoshwaran <180425518+cyber-country@users.noreply.github.com>
2026-03-26 06:14:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from model import model_train
 import numpy as np
 model,scaler,accuracy,c=model_train()
 st.title("Exam prediction")
-st.write("This model tell us pass/fail using multi feature and using logistic regression")
+st.write("st.write("Predict whether a student will Pass or Fail based on study hours and previous marks.")")
 st.write("The model accuracy is :-",accuracy)
 st.write("The model confusion matrix:-",c)
 print(c)
@@ -13,7 +13,7 @@ if st.button("Predict"):
     data=np.array([[hour,prev]])
     data=scaler.transform(data)
     ouput=model.predict(data)
-    if ouput==1:
+    if ouput[0]==1:
         ouput="Pass"
     else:
         ouput="Fail"
