9aaf8008184c8f65989d94e980bc17ce81438d65
R Santoshwaran <180425518+cyber-country@users.noreply.github.com>
2026-03-26 06:15:39 -0700

    Refactor app.py for improved readability

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from model import model_train
 import numpy as np
 model,scaler,accuracy,c=model_train()
 st.title("Exam prediction")
-st.write("st.write("Predict whether a student will Pass or Fail based on study hours and previous marks.")")
+st.write("Predict whether a student will Pass or Fail based on study hours and previous marks.")
 st.write("The model accuracy is :-",accuracy)
 st.write("The model confusion matrix:-",c)
 print(c)
